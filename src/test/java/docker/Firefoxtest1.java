c9fd41476ad63d74309ec08551dd51822fe894a4
Harini D <harini.d01@ad.infosys.com>
2020-09-11 17:15:04 +0530

    first commit

--- src/test/java/docker/Firefoxtest1.java
+++ src/test/java/docker/Firefoxtest1.java
@@ -1,27 +1,63 @@
 package docker;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-public class Firefoxtest1 {
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterTest;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+
+
+public class Firefoxtest1 {
 	
+	RemoteWebDriver driver;
 	
-@Test
-	
-	public static void grid2() throws MalformedURLException{
-		DesiredCapabilities dc=DesiredCapabilities.firefox();
-				
-				URL url=new URL("http://192.168.99.100:4545/wd/hub");
-				
-				RemoteWebDriver driver=new RemoteWebDriver(url,dc);
-				driver.get("https://opensource-demo.orangehrmlive.com/");
-				System.out.println("title of page"+driver.getTitle());
-				driver.quit();
-				
-			}
+	 @BeforeClass
+	  public void beforeClass() throws MalformedURLException {
+		 DesiredCapabilities dc=DesiredCapabilities.firefox();
+	        
+	        
+	        URL url=new URL("http://192.168.99.100:4545/wd/hub");
+	        
+	        driver=new RemoteWebDriver(url,dc);
+	        driver.get("https://opensource-demo.orangehrmlive.com/");
+	  }
+
+  @Test
+  public void f() {
+	  System.out.println("title of page : "+driver.getTitle());
+  }
+ @Test
+ public void f2() {
+	 driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+	 driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+	 driver.findElement(By.id("btnLogin")).click();
+	 String url= driver.getCurrentUrl();
+	 System.out.println("Current page url is : "+url);
+ }
+ @Test
+ public void f3() throws InterruptedException{
+	 driver.findElement(By.xpath("//*[@id=\"menu_dashboard_index\"]/b")).click();
+	 String txt=driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/h1")).getText();
+	 if(txt=="Dashboard") {
+		 System.out.println("Page exist");
+	 }
+	 else {
+		 System.out.println("page not exist");
+	 }
+	 
+ }
+  @AfterTest
+  public void afterTest(){
+	  driver.quit();
+  }
+
 }
--- src/test/java/docker/SleniumwithChrome.java
+++ src/test/java/docker/SleniumwithChrome.java
@@ -1,29 +1,68 @@
 package docker;
 
+import org.testng.annotations.Test;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterTest;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.Test;
+
+
 
 public class SleniumwithChrome {
 	
-	@Test
-	public static void grid1() throws MalformedURLException{
-DesiredCapabilities dc=DesiredCapabilities.chrome();
-		
-		URL url=new URL("http://192.168.99.100:4545/wd/hub");
-		
-		RemoteWebDriver driver=new RemoteWebDriver(url,dc);
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-		System.out.println("title of page"+driver.getTitle());
-		driver.quit();
-		
-	}
-
+	RemoteWebDriver driver;
 	
+	 @BeforeClass
+	  public void beforeClass() throws MalformedURLException {
+		 DesiredCapabilities dc=DesiredCapabilities.chrome();
+	        
+	        
+	        URL url=new URL("http://192.168.99.100:4545/wd/hub");
+	        
+	        driver=new RemoteWebDriver(url,dc);
+	        driver.get("https://opensource-demo.orangehrmlive.com/");
+	  }
 
-	
+  @Test
+  public void f() {
+	  System.out.println("title of page : "+driver.getTitle());
+  }
+  @Test
+  public void f2() {
+	  driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		 driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		 driver.findElement(By.id("btnLogin")).click();
+	  List<WebElement> links = driver.findElements(By.tagName("a"));
+	  
+	  System.out.println(links.size());
+	  
+	  for (int i = 1; i<links.size(); i=i+1)
+	  
+	  {
+	  
+	  System.out.println(links.get(i).getText());
+	  
+	  }
+  }
+  @Test
+  public void f3() {
+	  driver.findElement(By.id("welcome")).click();
+	  driver.findElement(By.id("aboutDisplayLink")).click();
+	  String msg=driver.findElement(By.tagName("body")).getText();
+	  System.out.println(msg);
+	  
+  }
+ 
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
 
 }
